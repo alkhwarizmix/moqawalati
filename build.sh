3fec59f1215657bfe5520c0631e26e0c687b1170
فارس بلحواس <fares.belhaouas@alkhwarizmix.com>
2014-04-23 23:40:19 -0400

    Added alkhwarizmixMavenRunAnt to build

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ echo FLASH_PLAYER_COMMAND=$FLASH_PLAYER_COMMAND
 MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 echo MAVEN_OPTS=$MAVEN_OPTS
 
-cd ../frameworks/source/
+cd ../frameworks/source/alkhwarizmixMavenRunAnt
+mvn clean install
+cd ..
 mvn -f alkhwarizmix-common.pom.xml clean install
-mvn -f install-PureMVC_AS3_MultiCore_Framework.pom.xml antrun:run install:install-file -Djsse.enableSNIExtension=false -DgroupId=org.puremvc -DartifactId=PureMVC_AS3_MultiCore_Framework -Dversion=1.0.5 -Dpackaging=swc -Dfile=target/download/PureMVC_AS3_MultiCore_1_0_5.swc
-mvn -f install-as3crypto.pom.xml antrun:run install:install-file -Djsse.enableSNIExtension=false -DgroupId=com.hurlant.crypto -DartifactId=as3crypto -Dversion=1.3 -Dpackaging=swc -Dfile=target/download/as3crypto.swc
+mvn -f install-PureMVC_AS3_MultiCore_Framework.pom.xml install install:install-file -Djsse.enableSNIExtension=false -DgroupId=org.puremvc -DartifactId=PureMVC_AS3_MultiCore_Framework -Dversion=1.0.5 -Dpackaging=swc -Dfile=target/download/PureMVC_AS3_MultiCore_1_0_5.swc
+mvn -f install-as3crypto.pom.xml install install:install-file -Djsse.enableSNIExtension=false -DgroupId=com.hurlant.crypto -DartifactId=as3crypto -Dversion=1.3 -Dpackaging=swc -Dfile=target/download/as3crypto.swc
 
 cd alkhwarizmixFlexFramework
 mvn clean install -Dflex.flashPlayer.command="$FLASH_PLAYER_COMMAND"
