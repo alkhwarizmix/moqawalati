6c693bf22dba072af90d2543e0bed7093b9cbb28
فارس بلحواس <fares.belhaouas@alkhwarizmix.com>
2015-10-28 22:17:42 -0400

    Added JUnit for WinrakRoads

---
+++ source/moqawalatiWeb/src/test/java/dz/alkhwarizmix/winrak/java/maps/WinrakRoadsTest.java
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+//  بسم الله الرحمن الرحيم
+//
+//  حقوق التأليف والنشر ١٤٣٧ هجري، فارس بلحواس (Copyright 2015 Fares Belhaouas)
+//  كافة الحقوق محفوظة (All Rights Reserved)
+//
+//  NOTICE: Fares Belhaouas permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package dz.alkhwarizmix.winrak.java.maps;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import dz.alkhwarizmix.framework.java.AlKhwarizmixException;
+
+/**
+ * <p>
+ * TODO: Javadoc
+ * </p>
+ *
+ * @author فارس بلحواس (Fares Belhaouas)
+ * @since ١٦ محرم ١٤٣٧ (October 28, 2015)
+ */
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("PMD.MethodNamingConventions")
+public class WinrakRoadsTest {
+
+	// --------------------------------------------------------------------------
+	//
+	// Setup & Teardown
+	//
+	// --------------------------------------------------------------------------
+
+	@InjectMocks
+	private WinrakRoads utWinrakRoads;
+
+	// --------------------------------------------------------------------------
+	//
+	// Helpers
+	//
+	// --------------------------------------------------------------------------
+
+	// EMPTY
+
+	// --------------------------------------------------------------------------
+	//
+	// Tests
+	//
+	// --------------------------------------------------------------------------
+
+	@Test
+	public void test00_constructor() throws AlKhwarizmixException {
+		Assert.assertNotNull(utWinrakRoads);
+	}
+
+	@Ignore("TODO: TDD")
+	@Test
+	public void test01() throws AlKhwarizmixException {
+		Assert.assertTrue(false);
+	}
+
+} // Class
