f44050d733af9ff4d3546fd7077f0afa63b4a039
فارس بلحواس <fares.belhaouas@alkhwarizmix.com>
2016-01-05 17:55:37 -0500

    Save WinrakService

--- source/moqawalatiWeb/src/test/java/dz/alkhwarizmix/winrak/java/services/impl/WinrakServiceTest.java
+++ source/moqawalatiWeb/src/test/java/dz/alkhwarizmix/winrak/java/services/impl/WinrakServiceTest.java
@@ -11,12 +11,20 @@
 
 package dz.alkhwarizmix.winrak.java.services.impl;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import dz.alkhwarizmix.framework.java.AlKhwarizmixException;
 
@@ -28,7 +36,8 @@ import dz.alkhwarizmix.framework.java.AlKhwarizmixException;
  * @author فارس بلحواس (Fares Belhaouas)
  * @since ١٧ ربيع الاول ١٤٣٧ (December 28, 2015)
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 @SuppressWarnings("PMD.MethodNamingConventions")
 public class WinrakServiceTest {
 
@@ -38,8 +47,17 @@ public class WinrakServiceTest {
 	//
 	// --------------------------------------------------------------------------
 
-	@InjectMocks
+	@Autowired
 	private WinrakService utWinrakService;
+
+	@Mock
+	private GoogleGeoApiWrapper mockGoogleGeoApiWrapper;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		utWinrakService.setGoogleGeoApiWrapper(mockGoogleGeoApiWrapper);
+	}
 
 	// --------------------------------------------------------------------------
 	//
@@ -60,9 +78,26 @@ public class WinrakServiceTest {
 		Assert.assertNotNull(utWinrakService);
 	}
 
+	@Test
+	public void test01_convertPositionToAddress_should_be_cacheable()
+			throws AlKhwarizmixException {
+		final String expected = "1234, My Address";
+		doReturn(expected).when(mockGoogleGeoApiWrapper)
+				.convertPositionToAddress(1.1, 2.2, 3000);
+		String result = utWinrakService
+				.convertPositionToAddress(1.1, 2.2, 3000);
+		Assert.assertEquals(expected, result);
+		verify(mockGoogleGeoApiWrapper, times(1)).convertPositionToAddress(1.1,
+				2.2, 3000);
+		result = utWinrakService.convertPositionToAddress(1.1, 2.2, 3000);
+		Assert.assertEquals(expected, result);
+		verify(mockGoogleGeoApiWrapper, times(1)).convertPositionToAddress(1.1,
+				2.2, 3000);
+	}
+
 	@Ignore("TODO: TDD")
 	@Test
-	public void test01() throws AlKhwarizmixException {
+	public void testXY() throws AlKhwarizmixException {
 		Assert.assertTrue(false);
 	}
 
