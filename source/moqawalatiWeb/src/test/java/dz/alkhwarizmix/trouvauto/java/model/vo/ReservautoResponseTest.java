f32f9cad7bd575f56832e3572f42ec2b91827e07
فارس بلحواس <fares.belhaouas@alkhwarizmix.com>
2016-01-24 20:40:49 -0500

    Issue: Coverage dz.alkhwarizmix.trouvauto.java.model.vo < 70

--- source/moqawalatiWeb/src/test/java/dz/alkhwarizmix/trouvauto/java/model/vo/ReservautoResponseTest.java
+++ source/moqawalatiWeb/src/test/java/dz/alkhwarizmix/trouvauto/java/model/vo/ReservautoResponseTest.java
@@ -11,14 +11,22 @@
 
 package dz.alkhwarizmix.trouvauto.java.model.vo;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import dz.alkhwarizmix.framework.java.AlKhwarizmixException;
+import dz.alkhwarizmix.framework.java.domain.AbstractAlKhwarizmixDomainObject;
+import dz.alkhwarizmix.framework.java.utils.DateUtil;
 import dz.alkhwarizmix.framework.java.utils.impl.JSONUtil;
 
 /**
@@ -43,6 +51,18 @@ public class ReservautoResponseTest {
 
 	private ReservautoResponse utReservautoResponse;
 
+	@BeforeClass
+	static public void static_setUp() {
+		final DateUtil mockDateUtil = mock(DateUtil.class);
+		when(mockDateUtil.newDate()).thenReturn(new Date(1234));
+		AbstractAlKhwarizmixDomainObject.dateUtil = mockDateUtil;
+	}
+
+	@AfterClass
+	static public void static_tearDown() {
+		AbstractAlKhwarizmixDomainObject.dateUtil = null;
+	}
+
 	@Before
 	public void setUp() {
 		utReservautoResponse = new ReservautoResponse();
@@ -54,7 +74,28 @@ public class ReservautoResponseTest {
 	//
 	// --------------------------------------------------------------------------
 
-	// EMPTY
+	private void setDataForReservautoResponseWithId(
+			final ReservautoResponse response, final int id) {
+		response.setId(new Long(id));
+		response.setUserPosition(new ReservautoPosition(1.2 * id, 1.2 * id));
+		final ReservautoVehiculeList vehicules = new ReservautoVehiculeList();
+		final ReservautoVehicule vehicule = new ReservautoVehicule();
+		vehicule.setPosition(new ReservautoPosition(1.3 * id, 1.3 * id));
+		vehicules.add(vehicule);
+		response.setVehicules(vehicules);
+	}
+
+	private void assertEqualReservautoResponses(
+			final ReservautoResponse expected, final ReservautoResponse clone,
+			final boolean testDeep) {
+		Assert.assertEquals("id", expected.getId(), clone.getId());
+		Assert.assertEquals("ExtensionData", expected.getExtensionData(),
+				clone.getExtensionData());
+		Assert.assertEquals("UserPosition", expected.getUserPosition(),
+				clone.getUserPosition());
+		Assert.assertEquals("Vehicules", expected.getVehicules(),
+				clone.getVehicules());
+	}
 
 	// --------------------------------------------------------------------------
 	//
@@ -65,6 +106,18 @@ public class ReservautoResponseTest {
 	@Test
 	public void test00_constructor() throws AlKhwarizmixException {
 		Assert.assertNotNull(utReservautoResponse);
+	}
+
+	@Test
+	public void test00_B_Cloneable() {
+		Assert.assertTrue(utReservautoResponse instanceof Cloneable);
+	}
+
+	@Test
+	public void test00_C_equals_empty_instance() {
+		final ReservautoResponse obj1 = new ReservautoResponse();
+		final ReservautoResponse obj2 = new ReservautoResponse();
+		Assert.assertTrue(obj1.equals(obj2));
 	}
 
 	@Test
@@ -92,10 +145,94 @@ public class ReservautoResponseTest {
 		Assert.assertEquals(4.5, result.getUserPosition().getLng(), 0);
 	}
 
-	@Ignore("TODO: TDD")
 	@Test
-	public void testXY() throws AlKhwarizmixException {
-		Assert.assertTrue(false);
+	public void test03_A_ExtensionData_setAndGet() {
+		final ReservautoExtensionData valueToSet = new ReservautoExtensionData();
+		utReservautoResponse.setExtensionData(valueToSet);
+		Assert.assertEquals(valueToSet, utReservautoResponse.getExtensionData());
+	}
+
+	@Test
+	public void test03_B_ExtensionData_equals() {
+		final ReservautoResponse obj1 = new ReservautoResponse();
+		final ReservautoResponse obj2 = new ReservautoResponse();
+		final ReservautoExtensionData reservautoExtensionData = mock(ReservautoExtensionData.class);
+		obj1.setExtensionData(reservautoExtensionData);
+		Assert.assertFalse(obj1.equals(obj2));
+		obj2.setExtensionData(reservautoExtensionData);
+		Assert.assertTrue(obj1.equals(obj2));
+	}
+
+	@Test
+	public void test04_A_UserPosition_setAndGet() {
+		final ReservautoPosition valueToSet = mock(ReservautoPosition.class);
+		utReservautoResponse.setUserPosition(valueToSet);
+		Assert.assertEquals(valueToSet, utReservautoResponse.getUserPosition());
+	}
+
+	@Test
+	public void test04_B_UserPosition_equals() {
+		final ReservautoResponse obj1 = new ReservautoResponse();
+		final ReservautoResponse obj2 = new ReservautoResponse();
+		final ReservautoPosition userPosition = new ReservautoPosition(1.2, 3.4);
+		obj1.setUserPosition(userPosition);
+		Assert.assertFalse(obj1.equals(obj2));
+		obj2.setUserPosition(userPosition);
+		Assert.assertTrue(obj1.equals(obj2));
+	}
+
+	@Test
+	public void test05_A_Vehicules_setAndGet() {
+		final ReservautoVehiculeList valueToSet = mock(ReservautoVehiculeList.class);
+		utReservautoResponse.setVehicules(valueToSet);
+		Assert.assertEquals(valueToSet, utReservautoResponse.getVehicules());
+	}
+
+	@Test
+	public void test05_B_Vehicules_equals() {
+		final ReservautoResponse obj1 = new ReservautoResponse();
+		final ReservautoResponse obj2 = new ReservautoResponse();
+		final ReservautoVehiculeList vehicules = mock(ReservautoVehiculeList.class);
+		obj1.setVehicules(vehicules);
+		Assert.assertFalse(obj1.equals(obj2));
+		obj2.setVehicules(vehicules);
+		Assert.assertTrue(obj1.equals(obj2));
+	}
+
+	@Test
+	public void test06_A_clone_null_properties() {
+		// SetUp
+		final ReservautoResponse expected = new ReservautoResponse();
+		utReservautoResponse = new ReservautoResponse();
+		// Test
+		final ReservautoResponse clone = (ReservautoResponse) utReservautoResponse
+				.clone();
+		// Others
+		setDataForReservautoResponseWithId(utReservautoResponse, 1567);
+		// Asserts
+		assertEqualReservautoResponses(expected, clone, false);
+	}
+
+	@Test
+	public void test06_B_clone() {
+		// SetUp
+		final ReservautoResponse expected = new ReservautoResponse();
+		setDataForReservautoResponseWithId(expected, 7651);
+		setDataForReservautoResponseWithId(utReservautoResponse, 7651);
+		// Test
+		final ReservautoResponse clone = (ReservautoResponse) utReservautoResponse
+				.clone();
+		// Others
+		setDataForReservautoResponseWithId(utReservautoResponse, 1569);
+		// Asserts
+		assertEqualReservautoResponses(expected, clone, true);
+	}
+
+	@Test
+	public void test07_hashCode() {
+		Assert.assertEquals(1705692209, utReservautoResponse.hashCode());
+		setDataForReservautoResponseWithId(utReservautoResponse, 7953);
+		Assert.assertEquals(1887784336, utReservautoResponse.hashCode());
 	}
 
 } // Class
